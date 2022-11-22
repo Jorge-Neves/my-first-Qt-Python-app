016bb2783dc0c405d6a0790bb6fb04a641b27d5e
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-11-22 18:22:13 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import sys
 import random
 from PySide6 import QtCore, QtWidgets, QtGui
 
-class MyWidget(QWidgets.QWidget):
+class MyWidget(QtWidgets.QWidget):
     def __ini__(self):
         super().__init__()
 
@@ -20,3 +20,14 @@ class MyWidget(QWidgets.QWidget):
     @QtCore.Slot()
     def magic(self):
         self.text.setText(random.choice(self.hello))
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
+    
